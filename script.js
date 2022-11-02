db195889a4ce6418c7eacaa588d9fe2f1252e279
BrownBearr <viswan.shiva@gmail.com>
2022-11-01 21:15:15 -0400

    Eloquent 1-3 + JSway 6-8

--- script.js
+++ script.js
@@ -16,3 +16,59 @@
           return num1;
         }
       };
+
+
+
+
+/*Bank account chapter 6*/
+
+const account = {
+  owner: "James",
+  balance: 0,
+
+  credit(money){
+    this.balance +=amount;
+  }
+
+  describe() {
+    return `owner: ${this.owner}, balance: ${this.balance}`;
+  }
+}
+
+console.log(account.describe());
+account.credit(250);
+account.credit(-80);
+console.log(account.describe());
+
+
+/*ch 7s*/
+
+const values = [3, 11, 7, 2, 9, 10];
+
+let sum = 0;
+
+values.forEach(value => { sum += value;});
+
+console.log(sum);
+
+
+
+/*Chapter 8*/ 
+function wordInfo(word) {
+  console.log(`The word ${word} is ${word.length} characters long`);
+  console.log(`Its lowercase value is ${word.toLowerCase()}`);
+  console.log(`Its uppercase value is ${word.toUpperCase()}`);
+
+  for (let i = 0; i < word.length; i++) {
+      if (word[i] !== word[length - 1 - i]) {
+          return 'it is not a palindrome'
+      } else {
+          return 'it is a palindrome'
+      }
+  }
+}
+
+
+
+
+
